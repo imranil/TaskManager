3f225660711479b1adfd958eba2c6d744422eb33
Ranil <rnyuskaev@gmail.com>
2022-09-06 13:57:31 +0500

    попытался сделать код более читабельным

--- frontend/src/components/calendar/month/Month.jsx
+++ frontend/src/components/calendar/month/Month.jsx
@@ -4,53 +4,66 @@ import { NavLink } from "react-router-dom";
 import { setCurrentTask } from "../../../reducers/taskSlice";
 
 const Month = () => {
-    const loader = useSelector(state => state.app.loader)
+    const isLoading = useSelector(state => state.app.loader)
     const currentMonth = useSelector(state => state.calendar.currentMonth)
     const currentYear = useSelector(state => state.calendar.currentYear)
-    const tasks = useSelector(state => state.tasks.tasks)
     const daysOfWeek = Object.keys(useSelector(state => state.calendar.daysOfWeek))
+    const tasks = useSelector(state => state.tasks.tasks)
+
+    const WEEK_DAY_SUNDAY = 6;
+    const WEEK_DAY_MONDAY = 0;
 
     function createMonthGrid() {
         let date = new Date(currentYear, currentMonth, 1)
         let month = [], days = []
 
-        for (let i = 0; i < getDay(date); i++) {
-            days.push(<Day />);
-        }
+        fillArrayWithEmptyDays(days, WEEK_DAY_MONDAY, getDay(date) - 1)
 
-        while (date.getMonth() == currentMonth) {
-            let tasksOfDay = tasks.filter(task => task.deadline === new Date(currentYear, currentMonth, date.getDate()).toLocaleDateString('en-CA'))
+        while (date.getMonth() === currentMonth) {
+            let tasksOfDay = getTasksOfDay(date.getDate())
 
-            if (date.getFullYear() === new Date().getFullYear() && date.getMonth() === new Date().getMonth() && date.getDate() === new Date().getDate()) {
-                days.push(<Day key={date.getDate()} heading={date.getDate()} tasks={tasksOfDay} today={true} />)
-            } else {
-                days.push(<Day key={date.getDate()} heading={date.getDate()} tasks={tasksOfDay} />)
-            }
-            if (getDay(date) % 7 == 6) {
+            days.push(<Day key={date.getDate()} heading={date.getDate()} tasks={tasksOfDay} today={isToday(date)} />)
+
+            if (getDay(date) % 7 === WEEK_DAY_SUNDAY) {
                 month.push(<Week key={month.length} days={days} />)
                 days = []
             }
             date.setDate(date.getDate() + 1);
         }
 
-        if (getDay(date) != 0) {
-            for (let i = getDay(date); i < 7; i++) {
-                days.push(<Day />);
-            }
+        if (getDay(date) !== WEEK_DAY_MONDAY) {
+            fillArrayWithEmptyDays(days, getDay(date), WEEK_DAY_SUNDAY)
         }
 
         month.push(<Week key={month.length} days={days} />)
+
         return month
+    }
+
+    function fillArrayWithEmptyDays(array, initialValue, finalValue) {
+        for (let i = initialValue; i <= finalValue; i++) {
+            array.push(<Day />);
+        }
     }
 
     function getDay(date) {
         let day = date.getDay();
-        if (day === 0) day = 7;
+        if (day === 0) {
+            day = 7;
+        }
+
         return day - 1;
     }
 
+    function getTasksOfDay(currentDay) {
+        return tasks.filter(task => task.deadline === new Date(currentYear, currentMonth, currentDay).toLocaleDateString('en-CA'))
+    }
+
+    function isToday(date) {
+        return date.getFullYear() === new Date().getFullYear() && date.getMonth() === new Date().getMonth() && date.getDate() === new Date().getDate();
+    }
     
-    if (loader) {
+    if (isLoading) {
         return (
             <div className="loader">
                 <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
@@ -75,13 +88,13 @@ const Week = ({ days }) => {
 }
 
 const Day = (props) => {
-    const classes = props.today ? 'day today' : 'day'
     let tasks = []
     for (let key in props.tasks) {
         tasks.push(props.tasks[key])
     }
+
     return (
-        <div className={classes}>
+        <div className={props.today ? 'day today' : 'day'}>
             <div className="heading">{props.heading} {props.today && 'Сегодня'}</div>
             <ul className="task-list">
                 {tasks.length !== 0 &&
@@ -95,6 +108,7 @@ const Day = (props) => {
 const TaskOfDay = ({ task }) => {
     const dispatch = useDispatch()
     const priorities = useSelector(state => state.tasks.priorities)
+    
     return (
         <NavLink to={'/task/' + task.id} onClick={() => dispatch(setCurrentTask(task))} className="item">
             {task.name}
