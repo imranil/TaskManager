483372fbbd2057af5ac71b1ebaadaa06900e83d8
Ranil <rnyuskaev@gmail.com>
2022-09-18 08:15:39 +0500

    модель тэга и отношения

---
+++ backend/models/Tag.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('tag', {
+        userId: {
+            type: type.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: "id"
+            },
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        },
+        name: {
+            type: type.STRING,
+        },
+    });
+}
--- backend/models/index.js
+++ backend/models/index.js
@@ -5,6 +5,7 @@ const UserModel = require('./User');
 const TaskModel = require('./Task');
 const UserTaskModel = require('./UserTask');
 const InvitationModel = require('./Invitation');
+const TagModel = require('./Tag');
 
 
 const sequelize = new Sequelize(dbConfig.dbName, dbConfig.user, dbConfig.password, {
@@ -17,9 +18,12 @@ const User = UserModel(sequelize, Sequelize);
 const Task = TaskModel(sequelize, Sequelize);
 const UserTask = UserTaskModel(sequelize, Sequelize);
 const Invitation = InvitationModel(sequelize, Sequelize);
+const Tag = TagModel(sequelize, Sequelize);
 
 User.belongsToMany(Task, {through: UserTask});
 Task.belongsToMany(User, {through: UserTask});
+Tag.belongsToMany(Task, {through: 'tasktag'});
+Task.belongsToMany(Tag, {through: 'tasktag'})
 
 
 
@@ -33,5 +37,6 @@ module.exports = {
   Task,
   UserTask, 
   Invitation,
+  Tag,
   sequelize
 }
